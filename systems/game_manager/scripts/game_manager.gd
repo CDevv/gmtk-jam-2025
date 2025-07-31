d1f59d250f0fbf1825684eca8036044867875d48
CDevv <cdevyt@gmail.com>
2025-07-31 12:39:50 +0300

    Base level loading

--- systems/game_manager/scripts/game_manager.gd
+++ systems/game_manager/scripts/game_manager.gd
@@ -14,12 +14,19 @@ class_name GameManager
 @export var player_packed_scene: PackedScene
 
 func _ready() -> void:
+	add_level()
 	add_player(Vector2(0,0))
 
 func add_player(pos: Vector2) -> void:
 	var player_to_add: CharacterBody2D = load(player_packed_scene.resource_path).instantiate()
 	player_to_add.global_position = pos
 	player_holder.add_child(player_to_add)
+
+func add_level() -> void:
+	var level_name: String = "test_level_alpha"
+	var resource_full_path = str("res://levels/", level_name, ".tscn")
+	var level_scene = load(resource_full_path).instantiate()
+	level_holder.add_child(level_scene)
 
 func request_level_to_load() -> void:
 	pass
