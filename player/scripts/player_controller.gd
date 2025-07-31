944dd44389ff2f9c2009fb550eec55c3ac0eedda
Jadus Developus <jadethemag3@proton.me>
2025-07-31 13:54:18 -0500

    possible fix?

--- player/scripts/player_controller.gd
+++ player/scripts/player_controller.gd
@@ -1,25 +1,58 @@
 class_name Player
 extends CharacterBody2D
 
-@export var gravity = 2500
-@export var move_change = 200
-@export var jump_change = 500
+@export_category('Speed and power variables')
+@export var gravity = 1500
+@export var move_speed = 200
+@export var jump_velocity = 500
 
-func input() -> void:
-	velocity.x = 0
-	
-	var right_press = Input.is_action_pressed("move right")
-	var left_press = Input.is_action_pressed("move left")
-	var jump_press = Input.is_action_pressed("jump")
-	
-	if is_on_floor() and jump_press:
-		velocity.y = -jump_change
-	if left_press:
-		velocity.x -= move_change
-	if right_press:
-		velocity.x += move_change
+@export_category('Exported node references')
+@export var player_visuals: Node2D
+
+var has_wings: bool = true
+
+func _get_input_direction() -> Vector2:
+	var dir: Vector2 = Vector2(Input.get_axis("move left", "move right"), 0)
+	return dir
+
+func _air_physics(delta: float) -> void:
+	var dir: Vector2 = _get_input_direction()
+
+	if dir:
+		velocity.x = lerp(velocity.x, dir.x * (move_speed * 2.0), delta * 7.25)
+	else:
+		velocity.x = move_toward(velocity.x, 0.0, delta * (move_speed * 0.75))
+
+	if Input.is_action_pressed("jump") and !is_on_floor() and has_wings:
+		velocity.y += -(gravity * 3.25) * delta
+
+	velocity.y = clamp(velocity.y, -500, 600)
+	velocity.y += gravity * delta
+
+	_directional_tilt(delta)
+
+func _ground_physics(delta: float) -> void:
+	var dir: Vector2 = _get_input_direction()
+
+	if dir:
+		velocity.x = lerp(velocity.x, dir.x * move_speed, delta * 9.5)
+	else:
+		velocity.x = move_toward(velocity.x, 0.0, delta * (move_speed * 2.5))
+
+	if Input.is_action_pressed("jump"):
+		velocity.y = -jump_velocity
+
+	if player_visuals.rotation != 0.0:
+		player_visuals.rotation = 0.0
+
+func _directional_tilt(delta: float) -> void:
+	player_visuals.rotation = lerp(player_visuals.rotation, velocity.x * 0.015, delta * 2.5)
+	player_visuals.rotation = clamp(player_visuals.rotation, -deg_to_rad(12), deg_to_rad(12))
 
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	input()
+	Game.game_manager._move_cam_to_target(self)
+	if is_on_floor():
+		_ground_physics(delta)
+	else:
+		_air_physics(delta)
 	move_and_slide()
